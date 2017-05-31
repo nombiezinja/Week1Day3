5a36a1836ab84a1ae850eebf2cb862da6471cbdb
Ti Zhang <tizhang8835@gmail.com>
2017-05-31 18:53:16 +0000

    Adds character-counting project file

---
+++ character-counting/character-counting.js
@@ -0,0 +1,16 @@
+// Signature:
+//   Name: countChar
+//   Arguments: strings
+//   Return type: object
+// Contract:
+//   Given a sring, returns an object whose keys are the
+//   individual letters of strings in the input, and whose
+//   values are the number of occurrences of each letter
+
+// Need to cycle through the string, record each letter
+// as it occurs, and add 1 to the counter each time it occurrs
+
+function countChar(str) {
+  output = {}
+  return output
+}
