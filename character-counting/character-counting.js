952cb4085cc902871124fbfb0ee9d1c95b8c50bd
Ti Zhang <tizhang8835@gmail.com>
2017-05-31 20:26:33 +0000

    Adds character-counting exercise

--- character-counting/character-counting.js
+++ character-counting/character-counting.js
@@ -10,7 +10,26 @@
 // Need to cycle through the string, record each letter
 // as it occurs, and add 1 to the counter each time it occurrs
 
+// Declare empty object
+// Use charAt  to cycle through string
+//***problem: can you use forEach on strings or just arrays?
+// convert string into array?
+// if letter does not exist, create new key with value of 1
+// if letter already exists as key, add 1 to its value
+// return the object
+
+//var str = process.argv.slice(2)
+//note: come back later and add .toString so can process
+//argument passed in node as one string
+
 function countChar(str) {
-  output = {}
-  return output
+  var strNoSpaces = str.split(" ").join("");
+  console.log(strNoSpaces);
+  output = {};
+  for (var i = 0; i < strNoSpaces.length; i++) {
+    output[strNoSpaces[i]] = strNoSpaces.split(strNoSpaces[i]).length-1;
+  }
+  return output;
 }
+
+console.log(countChar("assassins creed"));
